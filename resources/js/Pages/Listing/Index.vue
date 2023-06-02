69a7e9206ef004bb490ee7bc5d8b397fd0514d3d
Prasun Nandy Pritam <pritamnandy84@yahoo.com>
2023-06-02 23:55:52 +0600

    2nd Jun

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div v-for="listing in listings" :key="listing.id">
+            {{ listing.city }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        listings: Array,
+    })
+</script>
